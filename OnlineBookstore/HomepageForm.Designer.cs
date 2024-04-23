6c97d6631585be14b4e2a68a4792eca98e2da3d2
LukeCMoeller <lcmoeller@ksu.edu>
2024-04-23 16:41:56 -0500

    push

--- OnlineBookstore/HomepageForm.Designer.cs
+++ OnlineBookstore/HomepageForm.Designer.cs
@@ -28,32 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            SuspendLayout();
+            this.uxBookList = new System.Windows.Forms.ListBox();
+            this.uxBuyList = new System.Windows.Forms.ListBox();
+            this.e = new System.Windows.Forms.Label();
+            this.uxDisplaying = new System.Windows.Forms.Label();
+            this.uxPrice = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.uxBuy = new System.Windows.Forms.Button();
+            this.uxAdd = new System.Windows.Forms.Button();
+            this.uxRemove = new System.Windows.Forms.Button();
+            this.uxSearchButton = new System.Windows.Forms.Button();
+            this.uxSearchBox = new System.Windows.Forms.TextBox();
+            this.uxFilter = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
             // 
-            // label1
+            // uxBookList
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(304, 181);
-            label1.Name = "label1";
-            label1.Size = new Size(169, 20);
-            label1.TabIndex = 0;
-            label1.Text = "THIS IS THE HOMEPAGE";
+            this.uxBookList.FormattingEnabled = true;
+            this.uxBookList.ItemHeight = 15;
+            this.uxBookList.Location = new System.Drawing.Point(39, 39);
+            this.uxBookList.Name = "uxBookList";
+            this.uxBookList.Size = new System.Drawing.Size(163, 214);
+            this.uxBookList.TabIndex = 0;
+            this.uxBookList.SelectedIndexChanged += new System.EventHandler(this.uxBookList_SelectedIndexChanged);
             // 
-            // Form2
+            // uxBuyList
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
-            Name = "Form2";
-            Text = "Form2";
-            ResumeLayout(false);
-            PerformLayout();
+            this.uxBuyList.FormattingEnabled = true;
+            this.uxBuyList.ItemHeight = 15;
+            this.uxBuyList.Location = new System.Drawing.Point(241, 39);
+            this.uxBuyList.Name = "uxBuyList";
+            this.uxBuyList.Size = new System.Drawing.Size(120, 94);
+            this.uxBuyList.TabIndex = 1;
+            this.uxBuyList.SelectedIndexChanged += new System.EventHandler(this.uxBuyList_SelectedIndexChanged);
+            // 
+            // e
+            // 
+            this.e.AutoSize = true;
+            this.e.Location = new System.Drawing.Point(274, 21);
+            this.e.Name = "e";
+            this.e.Size = new System.Drawing.Size(67, 15);
+            this.e.TabIndex = 2;
+            this.e.Text = "Order Table";
+            // 
+            // uxDisplaying
+            // 
+            this.uxDisplaying.AutoSize = true;
+            this.uxDisplaying.Location = new System.Drawing.Point(110, 21);
+            this.uxDisplaying.Name = "uxDisplaying";
+            this.uxDisplaying.Size = new System.Drawing.Size(0, 15);
+            this.uxDisplaying.TabIndex = 3;
+            this.uxDisplaying.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uxPrice
+            // 
+            this.uxPrice.AutoSize = true;
+            this.uxPrice.Location = new System.Drawing.Point(224, 136);
+            this.uxPrice.Name = "uxPrice";
+            this.uxPrice.Size = new System.Drawing.Size(0, 15);
+            this.uxPrice.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(241, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Search:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(241, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Search by:";
+            // 
+            // uxBuy
+            // 
+            this.uxBuy.Location = new System.Drawing.Point(367, 107);
+            this.uxBuy.Name = "uxBuy";
+            this.uxBuy.Size = new System.Drawing.Size(74, 28);
+            this.uxBuy.TabIndex = 7;
+            this.uxBuy.Text = "Buy";
+            this.uxBuy.UseVisualStyleBackColor = true;
+            this.uxBuy.Click += new System.EventHandler(this.uxBuy_Click);
+            // 
+            // uxAdd
+            // 
+            this.uxAdd.Location = new System.Drawing.Point(367, 39);
+            this.uxAdd.Name = "uxAdd";
+            this.uxAdd.Size = new System.Drawing.Size(74, 28);
+            this.uxAdd.TabIndex = 8;
+            this.uxAdd.Text = "Add";
+            this.uxAdd.UseVisualStyleBackColor = true;
+            this.uxAdd.Click += new System.EventHandler(this.uxAdd_Click);
+            // 
+            // uxRemove
+            // 
+            this.uxRemove.Location = new System.Drawing.Point(367, 73);
+            this.uxRemove.Name = "uxRemove";
+            this.uxRemove.Size = new System.Drawing.Size(74, 28);
+            this.uxRemove.TabIndex = 9;
+            this.uxRemove.Text = "Remove";
+            this.uxRemove.UseVisualStyleBackColor = true;
+            this.uxRemove.Click += new System.EventHandler(this.uxRemove_Click);
+            // 
+            // uxSearchButton
+            // 
+            this.uxSearchButton.Location = new System.Drawing.Point(340, 230);
+            this.uxSearchButton.Name = "uxSearchButton";
+            this.uxSearchButton.Size = new System.Drawing.Size(75, 23);
+            this.uxSearchButton.TabIndex = 10;
+            this.uxSearchButton.Text = "Search";
+            this.uxSearchButton.UseVisualStyleBackColor = true;
+            this.uxSearchButton.Click += new System.EventHandler(this.uxSearchButton_Click);
+            // 
+            // uxSearchBox
+            // 
+            this.uxSearchBox.Location = new System.Drawing.Point(320, 153);
+            this.uxSearchBox.Name = "uxSearchBox";
+            this.uxSearchBox.Size = new System.Drawing.Size(121, 23);
+            this.uxSearchBox.TabIndex = 11;
+            // 
+            // uxFilter
+            // 
+            this.uxFilter.FormattingEnabled = true;
+            this.uxFilter.Items.AddRange(new object[] {
+            "Title",
+            "Author",
+            "ISBN",
+            "Genre",
+            "Price",
+            "Publisher"});
+            this.uxFilter.Location = new System.Drawing.Point(320, 191);
+            this.uxFilter.Name = "uxFilter";
+            this.uxFilter.Size = new System.Drawing.Size(121, 23);
+            this.uxFilter.TabIndex = 12;
+            this.uxFilter.SelectedIndexChanged += new System.EventHandler(this.uxFilter_SelectedIndexChanged);
+            // 
+            // HomepageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.uxFilter);
+            this.Controls.Add(this.uxSearchBox);
+            this.Controls.Add(this.uxSearchButton);
+            this.Controls.Add(this.uxRemove);
+            this.Controls.Add(this.uxAdd);
+            this.Controls.Add(this.uxBuy);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.uxPrice);
+            this.Controls.Add(this.uxDisplaying);
+            this.Controls.Add(this.e);
+            this.Controls.Add(this.uxBuyList);
+            this.Controls.Add(this.uxBookList);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "HomepageForm";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.HomepageForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
 
-        private Label label1;
+        private ListBox uxBookList;
+        private ListBox uxBuyList;
+        private Label e;
+        private Label uxDisplaying;
+        private Label uxPrice;
+        private Label label4;
+        private Label label5;
+        private Button uxBuy;
+        private Button uxAdd;
+        private Button uxRemove;
+        private Button uxSearchButton;
+        private TextBox uxSearchBox;
+        private ComboBox uxFilter;
     }
 }
--- OnlineBookstore/HomepageForm.cs
+++ OnlineBookstore/HomepageForm.cs
@@ -7,14 +7,276 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Globalization;
+using Microsoft.VisualBasic;
+using System.Text.RegularExpressions;
 
 namespace OnlineBookstore
 {
     public partial class HomepageForm : Form
     {
+        private int _maxbook = 0;
         public HomepageForm()
         {
             InitializeComponent();
+
+        }
+
+
+
+        private void HomepageForm_Load(object sender, EventArgs e)
+        {
+
+            uxBuy.Enabled = false;
+            uxRemove.Enabled = false;
+            //disables buttons
+            //hids admin button if not admin
+
+
+            //cant test if this works but quite sure it dosnt. 
+            string connectionString = ConfigurationManager.ConnectionStrings["OnlineBookstoreDb"].ConnectionString;
+            string query =
+            @"SELECT 
+                b.Title AS BookTitle, 
+                a.AuthorName
+            FROM Authors a
+            INNER JOIN Books b ON a.AuthorID = b.AuthorID
+            ORDER BY b.Title";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string bookTitle = reader["BookTitle"].ToString();
+                        string AuthorID = reader["AuthorID"].ToString();
+
+                        uxBookList.Items.Add($"{bookTitle} by {AuthorID}");
+                    }
+                }
+            }
+            _maxbook = uxBookList.Items.Count; //gets max book count
+            uxDisplaying.Text = _maxbook + " of " + _maxbook;
+        }
+
+
+        private void uxSearchButton_Click(object sender, EventArgs e)
+        {
+            string searchterm = uxSearchBox.Text.ToString();
+            uxBookList.Items.Clear();
+            switch (uxFilter.SelectedItem.ToString())
+            {
+                case "Title":
+                    ExecuteQueryTitle(searchterm);
+                    break;
+                case "Author":
+                    ExecuteQueryAuthor(searchterm);
+                    break;
+                case "ISBN":
+                    ExecuteQueryISBN(searchterm);
+                    break;
+                case "Genre":
+                    ExecuteQueryGenre(searchterm);
+                    break;
+                case "Price":
+                    ExecuteQueryPrice(searchterm);
+                    break;
+                case "Publisher":
+                    ExecuteQueryPublisher(searchterm);
+                    break;
+                default:
+                    MessageBox.Show("Something went wrong");
+                    break;
+            }
+            uxDisplaying.Text = uxBookList.Items.Count + " of " + _maxbook;
+        }
+        private void uxBuy_Click(object sender, EventArgs e)
+        {
+
+            //in the future would open the buy form and somehow send all of the buylist to it
+        }
+        //important all sql queres here
+        #region Queries to complete
+        private void ExecuteQueryTitle(string filter)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["OnlineBookstoreDb"].ConnectionString;
+            string query =
+                @"QUERY";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader()) // getting ISBN error here
+                {
+                    while (reader.Read())
+                    {
+
+                        uxBookList.Items.Add($"");
+                    }
+                }
+            }
+        }
+        private void ExecuteQueryAuthor(string filter)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["OnlineBookstoreDb"].ConnectionString;
+            string query =
+                @"QUERY";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader()) // getting ISBN error here
+                {
+                    while (reader.Read())
+                    {
+
+                        uxBookList.Items.Add($"");
+                    }
+                }
+            }
+        }
+        private void ExecuteQueryISBN(string filter)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["OnlineBookstoreDb"].ConnectionString;
+            string query =
+                @"QUERY";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader()) // getting ISBN error here
+                {
+                    while (reader.Read())
+                    {
+
+                        uxBookList.Items.Add($"");
+                    }
+                }
+            }
+        }
+        private void ExecuteQueryGenre(string filter)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["OnlineBookstoreDb"].ConnectionString;
+            string query =
+                @"QUERY";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader()) // getting ISBN error here
+                {
+                    while (reader.Read())
+                    {
+
+                        uxBookList.Items.Add($"");
+                    }
+                }
+            }
+        }
+        private void ExecuteQueryPrice(string filter)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["OnlineBookstoreDb"].ConnectionString;
+            string query =
+                @"QUERY";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader()) // getting ISBN error here
+                {
+                    while (reader.Read())
+                    {
+
+                        uxBookList.Items.Add($"");
+                    }
+                }
+            }
+        }
+        private void ExecuteQueryPublisher(string filter)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["OnlineBookstoreDb"].ConnectionString;
+            string query =
+                @"QUERY";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader()) // getting ISBN error here
+                {
+                    while (reader.Read())
+                    {
+
+                        uxBookList.Items.Add($"");
+                    }
+                }
+            }
+        }
+        #endregion 
+        //important all sql queres here
+
+        #region ignore
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private void uxAdd_Click(object sender, EventArgs e)
+        {
+            uxBuyList.Items.Add(uxFilter.SelectedItem);
+            if (uxRemove.Enabled = false && uxBuyList.Items.Count >= 1)
+            {
+                uxRemove.Enabled = true;
+            }
+            updatePrice();
+        }
+        private void updatePrice()
+        {
+            double price = 0.0;
+            foreach (string item in uxBuyList.Items)
+            {
+                price += double.Parse(item.Substring(item.IndexOf('$') + 1));
+            }
+            uxPrice.Text = "Total: $" + price.ToString();
+        }
+        private void uxRemove_Click(object sender, EventArgs e)
+        {
+            if (uxBuyList.Items.Count - 1 <= 0)
+            {
+                uxRemove.Enabled = false;
+            }
+            updatePrice();
+        }
+
+        private void uxBuyList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (uxBuyList.Items.Count >= 1)//if nothing selected remove button disabled
+            {
+                uxRemove.Enabled = true;
+            }
+
         }
     }
 }
